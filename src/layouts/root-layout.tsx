b3ae24f5db16da648257fa1ccd9a6ec8f9ed1059
datnt19213 <datnguyen.frontend.dev@gmail.com>
2025-02-05 10:40:23 +0700

    feat: Add LegalButton

--- src/layouts/root-layout.tsx
+++ src/layouts/root-layout.tsx
@@ -1,8 +1,16 @@
-import { AppProvider } from "@/providers/app-provider";
-import { IGlobalLayoutProps } from "@/types";
+import Footer from "@/components/common/footer";
+import Header from "@/components/common/header";
+import {AppProvider} from "@/providers/app-provider";
+import {IGlobalLayoutProps} from "@/types";
 
-const GlobalRootLayout = ({ children }: IGlobalLayoutProps) => {
-  return <AppProvider>{children}</AppProvider>;
+const GlobalRootLayout = ({children}: IGlobalLayoutProps) => {
+  return (
+    <AppProvider>
+      <Header />
+      {children}
+      <Footer />
+    </AppProvider>
+  );
 };
 
 export default GlobalRootLayout;
